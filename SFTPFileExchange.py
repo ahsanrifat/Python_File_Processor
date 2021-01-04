5d97d822dcf7c6e2460c82fb4a319847de581aac
ahsanrifat <frifat2013@gmail.com>
2021-01-04 12:11:08 +0600

    Changed SFTP scriprt to functions

--- SFTPFileExchange.py
+++ SFTPFileExchange.py
@@ -1,22 +1,37 @@
 import paramiko
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname="127.0.0.1", username="test_user", password="test", port=22)
-sftp_client = ssh.open_sftp()
-sftp_client.chdir("/folder")
-print(sftp_client.getcwd())
 
-# downloading sftp server's file to local
-sftp_client.get(
-    "/folder/abc.xlsx",
-    "D:\Robi Work 2021\File Handling Algorithm/abc.xlsx",
-)
-# transferring local file to sftp
-sftp_client.put(
-    r"D:\Entertainment\Downloads\abc.xlsx",
-    "/folder/abc.xlsx",
-)
+def establish_connection():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname="127.0.0.1", username="test_user", password="test", port=22)
+    return ssh
 
-sftp_client.close()
-ssh.close()
+
+def get_sftp_client(ssh):
+    return ssh.open_sftp()
+
+
+def close_connection(ssh, sftp_client):
+    sftp_client.close()
+    ssh.close()
+
+
+def transfer_file(remote_file_path, local_file_path):
+    ssh = establish_connection()
+    sftp_client = get_sftp_client(ssh)
+    sftp_client.put(
+        r"{}".format(local_file_path),
+        r"{}".format(remote_file_path),
+    )
+    close_connection(ssh, sftp_client)
+
+
+def download_file(local_file_path, remote_file_path):
+    ssh = establish_connection()
+    sftp_client = get_sftp_client(ssh)
+    sftp_client.put(
+        r"{}".format(remote_file_path),
+        r"{}".format(local_file_path),
+    )
+    close_connection(ssh, sftp_client)
