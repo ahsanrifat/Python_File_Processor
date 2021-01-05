d9dffa9888e89e7cac63f9c1fcc42af03dc08a07
ahsanrifat <frifat2013@gmail.com>
2021-01-05 11:29:06 +0600

    Redigned System

--- FileProcessor.py
+++ FileProcessor.py
@@ -1,40 +1,33 @@
 import os
-
-parent_directory = ""
-success_directory = ""
-error_directory = ""
+import SFTPFileExchange as sftp
 
 
-def isDirectory(path):
+def get_file_from_server(
+    hostname, username, password, port, server_directory, parent_directory
+):
+    ssh = sftp.establish_connection(hostname, username, password, port)
+    sftp_client = sftp.get_sftp_client(ssh)
+    local_file_path = sftp.download_file(
+        ssh, sftp_client, parent_directory, server_directory
+    )
+    return local_file_path
+
+
+def process_file(parent_directory, success_directory, error_directory):
+    pass
+
+
+def is_directory(path):
     return os.path.exists(path)
 
 
-def setParentDirectory(directory_input):
-    global parent_directory
-    parent_directory = directory_input
-
-
-def setSuccessDirectory(directory_input):
-    global success_directory
-    success_directory = directory_input
-
-
-def setErrorDirectory(directory_input):
-    global error_directory
-    error_directory = directory_input
-
-
-def onProcessSuccess(file):
+def on_process_success(file):
     # save file to success directory
     # remove file from parent directory
     pass
 
 
-def onProcessError(file):
+def on_process_error(file):
     # save file to error directory
     # remove file from parent directory
-    pass
-
-
-def processFile(file):
     pass
--- SFTPFileExchange.py
+++ SFTPFileExchange.py
@@ -1,7 +1,15 @@
 import paramiko
+from datetime import datetime
 
 
-def establish_connection():
+def establish_connection(hostname, username, password, port):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=hostname, username=username, password=password, port=port)
+    return ssh
+
+
+def establish_connection_local():
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname="127.0.0.1", username="test_user", password="test", port=22)
@@ -17,8 +25,30 @@ def close_connection(ssh, sftp_client):
     ssh.close()
 
 
+def get_latest_file(remote_file_path, sftp_client):
+    latest = 0
+    latestfile = None
+    sftp_client.chdir(remote_file_path)
+    for fileattr in sftp_client.listdir_attr():
+        if fileattr.st_mtime > latest:
+            latest = fileattr.st_mtime
+            latestfile = fileattr.filename
+    print(datetime.fromtimestamp(latest))
+    return latestfile
+
+
+def download_file(ssh, sftp_client, local_file_path, remote_file_path):
+    latest_file_name = get_latest_file(remote_file_path, sftp_client)
+    local_file_path = r"{}/{}".format(local_file_path, latest_file_name)
+    sftp_client.get(
+        r"{}/{}".format(remote_file_path, latest_file_name), local_file_path
+    )
+    close_connection(ssh, sftp_client)
+    return local_file_path
+
+
 def transfer_file(remote_file_path, local_file_path):
-    ssh = establish_connection()
+    ssh = establish_connection_local()
     sftp_client = get_sftp_client(ssh)
     sftp_client.put(
         r"{}".format(local_file_path),
@@ -27,11 +57,7 @@ def transfer_file(remote_file_path, local_file_path):
     close_connection(ssh, sftp_client)
 
 
-def download_file(local_file_path, remote_file_path):
-    ssh = establish_connection()
-    sftp_client = get_sftp_client(ssh)
-    sftp_client.put(
-        r"{}".format(remote_file_path),
-        r"{}".format(local_file_path),
-    )
-    close_connection(ssh, sftp_client)
+# ssh = establish_connection_local()
+# sftp_client = get_sftp_client(ssh)
+# print((sftp_client.listdir("/folder")))
+# print(dir(sftp_client))
